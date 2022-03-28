7ed53d00350314304937b3f3db54d392c10c1ef9
PalomaAlma <palomavacheron@gmail.com>
2022-03-28 17:34:37 +0100

    Comment List per Post (Model & View & CSS)

--- src/Model/Post.php
+++ src/Model/Post.php
@@ -5,5 +5,11 @@ namespace App\Model;
 class Post extends Repository {
 
     protected $table = 'post';
+//    private $comments = array();
+
+    public function getComments(): array
+    {
+        return $this->read('SELECT c.* FROM comment c WHERE post_id = ?', [$this->id]);
+    }
 
 }
