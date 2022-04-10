c24f79ccd81e861b2eca6f4f8cff6467c07c1cef
PalomaAlma <palomavacheron@gmail.com>
2022-04-10 18:38:12 +0100

    Login Function Fix

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,11 +12,12 @@ class LoginController extends AbstractController {
     }
 
     public function login() {
-        $user = (new User($this->getDB()))->getByEmail('palomavacheron@gmail.com');
-        var_dump($user->password);
+        $user = (new User($this->getDB()))->getByEmail($_POST['email']);
+//        var_dump($user->password);
         if (password_verify($_POST['password'], $user->password)) {
+//            var_dump($user);
             $_SESSION['auth'] = $user->admin;
-            if ($user->admin === 1) {
+            if ($this->isAdmin()) {
                 return header('Location: /admin');
             }
             return header('Location: /posts');
