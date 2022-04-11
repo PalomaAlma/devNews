9c33175593482f2e215d3aa773bde74df104ccbb
PalomaAlma <palomavacheron@gmail.com>
2022-04-11 14:00:13 +0100

    Clean AbstractController.php

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -5,18 +5,21 @@ use Database\DBConnection;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-abstract class AbstractController {
+abstract class AbstractController
+{
 
     private FilesystemLoader $loader;
     protected Environment $twig;
     protected $db;
 
-    public function __construct(DBConnection $db) {
+    public function __construct(DBConnection $db)
+    {
         $this->loader = new FilesystemLoader('../View');
         $this->twig = new Environment($this->loader, [
             'cache' => false,
         ]);
         if (session_status() === PHP_SESSION_NONE)
+
         {
             session_start();
         }
@@ -24,16 +27,19 @@ abstract class AbstractController {
         $this->db = $db;
     }
 
-    protected function getDB() {
+    protected function getDB()
+    {
         return $this->db;
     }
 
-    protected function isAdmin() {
+    protected function isAdmin()
+    {
         if (isset($_SESSION['auth']) && $_SESSION['auth'] === '1')
+
         {
-//            var_dump($_SESSION['auth']); die();
             return true;
-        } else {
+        } else
+        {
             return header('Location: /posts');
         }
     }
